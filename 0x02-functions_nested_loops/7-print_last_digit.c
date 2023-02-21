756bb7370380d2b25939672146d4310310c2782d
caid10 <kingcaid101@gmail.com>
2023-02-21 09:42:23 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,17 +6,11 @@
  */
 int print_last_digit(int n)
 {
-	int a;	
+	int x;
 
 	if (n < 0)
 		n = -n;
-
-	a = n % 10;
-		
-	if (a < 0)
-		a = -a;
-	
-	_putchar(a + '0');
-	
-	return (a);
+	x = n % 10;
+	_putchar(x + '0');
+	return (x);
 }
